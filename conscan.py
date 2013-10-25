7d70257adee3c5749106183125a6cef99c6d3a50
TheXerocouk <webmaster@thexero.co.uk>
2013-10-25 15:00:22 +0100

    added beta files

---
+++ conscan.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+## import modules
+import argparse
+import httplib
+import sys
+from modules import concrete
+
+## Banner
+def banner():
+	print """
+//========================================================================\\\\
+||                                                                        ||
+|| TITLE                                                                  ||
+|| Concrete CMS blackbox vulnerability scanner              	          ||
+||                                                                        ||
+|| VERSION                                                                ||
+|| 0.5                                                                    ||
+||                                                                        ||
+|| AUTHOR                                                                 ||
+|| TheXero | thexero@nullsecurity.net                                     ||
+||                                                                        ||
+|| WEBSITE                                                                ||
+|| www.thexero.co.uk | www.nullsecurity.net                               ||
+||                                                                        ||
+\\\\========================================================================//
+    """
+
+## Argument Parser
+def arg_parser():
+	parser = argparse.ArgumentParser(add_help=True,
+	epilog='Example: ./%(prog)s -t https://www.thexero.co.uk:8443/concrete/ -e')
+
+	parser.add_argument('-t', dest='target', help='Target IP / Domain', required='yes')
+	parser.add_argument('-e', action='store_true', help='Perform enumeration')
+
+	if len(sys.argv) == 1:
+		parser.print_help()
+		sys.exit(2)
+
+	args = parser.parse_args()
+	target = args.target
+
+
+	if target.startswith("https://"):
+		ssl = True
+		target = target[8:]
+
+	elif target.startswith("http://"):
+		ssl = False
+		target = target[7:]
+
+	else:
+		ssl = False
+
+	if "/" in target:
+		temp = target.split("/")
+		target = temp[0]
+
+		length = len(temp)		
+		length = length 
+
+		temp = temp[1:]
+
+		dir = '/'
+		for item in temp:
+			dir = dir + item + '/' 
+
+	else:
+		dir = '/'	
+
+	if args.e:
+        	enumerate = True
+	else:
+        	enumerate = False
+
+	concrete.detect(target, dir, ssl)
+	
+	if enumerate == True:
+		concrete.enumerate(target, dir, ssl)
+
+
+## Program startup
+if __name__ == '__main__':
+
+	banner()
+	arg_parser()
+	sys.exit(0)
+
---
+++ modules/concrete.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+import httplib, string, sys
+from modules import cmsvulns
+
+bold = '\033[1m'
+normal = '\033[0m'
+
+## Dection function
+def detect(target, dir, ssl):
+    try:
+        if ssl == True: ## Enables SSL
+            conn = httplib.HTTPSConnection(target)
+            conn.request("GET", dir)
+            response = conn.getresponse()
+            data = response.read()
+            conn.close()
+
+        else: ## Uses plain-text HTTP
+            conn = httplib.HTTPConnection(target)
+            conn.request("GET", dir)
+            response = conn.getresponse()
+            data = response.read()
+            conn.close()
+
+        for line in string.split(data, '\n'):
+            if 'generator' in line:
+                ver = line.split("\"")
+                version = ver[3].split(" ")
+
+                if version[0] == 'concrete5':
+                    print "Found", version[0], "at version", version[2], "via generator tag"
+                    cmsvulns.vulncheck(version[2])
+		    break
+		else:
+		    print "Not running concrete5!"
+		    sys.exit(0)
+
+
+	if not "/concrete/css/" in data:
+		print "concrete5 installation not detected"
+		sys.exit(0)
+
+    except Exception, error:
+        print error
+        sys.exit(1)
+
+
+
+def enumerate(target, dir, ssl):
+	
+	fullpath(target, dir, ssl)
+
+
+def fullpath(target, dir, ssl):
+	try:
+
+        	if ssl == True: ## Enables SSL
+            		conn = httplib.HTTPSConnection(target)
+            		conn.request("GET", dir + "concrete/blocks/content/editor_config.php")
+            		response = conn.getresponse()
+            		data = response.read()
+            		conn.close()
+
+        	else: ## Uses plain-text HTTP
+            		conn = httplib.HTTPConnection(target)
+            		conn.request("GET", dir + "concrete/blocks/content/editor_config.php")
+            		response = conn.getresponse()
+            		data = response.read()
+            		conn.close()
+
+		for line in string.split(data, '\n'):
+			if 'Fatal error' in line:
+				line = line.split(" ")
+				line = line[8]
+				length = len(line)
+				length = length - 4
+				fpd = line[3:length]
+
+				print bold + "\nFull Path Disclosure found!\r" + normal
+				print fpd + "\n"
+
+	except Exception, error:
+		print error
+
+
